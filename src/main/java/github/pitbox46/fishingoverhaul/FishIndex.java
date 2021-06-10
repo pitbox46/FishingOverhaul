4a43696c959545fcd89c30bb30f49edabaf5098d
pitbox46 <37819783+pitbox46@users.noreply.github.com>
2021-06-09 19:50:53 -0700

    Redid resource system

--- src/main/java/github/pitbox46/fishingoverhaul/FishIndex.java
+++ src/main/java/github/pitbox46/fishingoverhaul/FishIndex.java
@@ -1,9 +1,18 @@
 package github.pitbox46.fishingoverhaul;
 
+import com.google.gson.*;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.lang.reflect.Type;
+
 public class FishIndex {
+    private final Item item;
     private final float catchChance;
     private final float variability;
-    public FishIndex(float catchChance, float variability) {
+    public FishIndex(Item item, float catchChance, float variability) {
+        this.item = item;
         this.catchChance = catchChance;
         this.variability = variability;
     }
@@ -14,5 +23,30 @@ public class FishIndex {
 
     public float getVariability() {
         return variability;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public static class Serializer implements JsonDeserializer<FishIndex>, JsonSerializer<FishIndex> {
+        public FishIndex deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            JsonObject obj = json.getAsJsonObject();
+            Item item = null;
+            if(obj.has("item")) {
+                item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(obj.get("item").getAsString()));
+            }
+            float catchChance = obj.get("catch_chance").getAsFloat();
+            float variability = obj.get("variability").getAsFloat();
+            return new FishIndex(item, catchChance, variability);
+        }
+
+        public JsonElement serialize(FishIndex src, Type typeOfSrc, JsonSerializationContext context) {
+            JsonObject obj = new JsonObject();
+            obj.addProperty("item", src.getItem().getRegistryName().toString());
+            obj.addProperty("catch_chance", src.getCatchChance());
+            obj.addProperty("variability", src.getVariability());
+            return obj;
+        }
     }
 }
